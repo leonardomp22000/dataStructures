6eff5ba2b6631ab19768536ae402510ec54f7cb5
Leonardo <leonardomp2200@gmail.com>
2025-03-01 17:13:31 -0600

    delete a node from an specific position done

--- LinkedList/insertAtEnd.js
+++ LinkedList/insertAtEnd.js
@@ -14,17 +14,63 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  deleteNode(position) {
+    let nextNode;
+    let current = this.head;
+    let counter = 0;
+    let prev;
+    if (this.head === null) {
+      return "La lista esta vacia";
+    }
+    if (position === 0) {
+      nextNode = current.next;
+      this.head = nextNode;
+    } else {
+      while (counter <= position) {
+        if (counter === position - 1) {
+          prev = current;
+        }
+        current = current.next;
+        counter++;
+      }
+      prev.next = current;
+    }
+  }
+
+  addSpecificPosition(data, position) {
+    const node = new Node(data);
+    if (position === 0 || this.head === null) {
+      node.next = this.head;
+      this.head = node;
+    } else {
+      let current = this.head; // Current no es una copia de head, si no que se hace una referencia a la memoria del objeto
+      let nextNode;
+      let counter = 1;
+      while (current) {
+        if (counter === position) {
+          break;
+        }
+        current = current.next; // No se afecta llist o en este caso head, ya que solo se cambia el puntero de lugar
+        counter++;
+      }
+      nextNode = current.next;
+      current.next = node; // En este caso ya que se accede a una propiedad de un nodo al cual current esta apuntando, es por eso que se modifica llist o en este caso la lista
+      node.next = nextNode;
+    }
+  }
   addFirst(data) {
     const node = new Node(data);
     node.next = this.head;
     this.head = node;
     return this.head;
   }
-
+  // Se usa una variable auxiliar en este caso current, para recorrer la lista, ya de esta forma se hace una copia del head. No se modifica la referencia inicial.
   addAtEnd(data) {
     const node = new Node(data);
     let current = this.head;
     while (current.next) {
+      //Se evalua el next la referencia del siguiente nodo, cuando la referencia es null, entonces se detiene y se inserta el siguiente nodo
       current = current.next;
     }
     current.next = node;
@@ -38,6 +84,7 @@ class LinkedList {
       return answer;
     }
     while (current) {
+      //Se evaluan todos los nodos de su valor next
       if (current.next) {
         answer += current.data + "->";
       } else {
@@ -58,8 +105,18 @@ list1.addFirst(40);
 list1.addFirst(50);
 
 list1.addAtEnd(80);
-
+list1.addSpecificPosition(4, 1);
+list1.deleteNode(3);
 console.log(list1.printList());
+
+{
+  /** Notas del ejercicio addSpecificPosition
+1️⃣ JavaScript maneja objetos por referencia, no por valor.
+2️⃣ current es solo una variable auxiliar que apunta a nodos dentro de llist, pero las modificaciones que haces en los nodos afectan a llist directamente.
+3️⃣ No retornamos current porque perderíamos la estructura completa de la lista.
+4️⃣ Retornamos llist porque es la cabeza de la lista y mantiene toda la estructura.
+  */
+}
 
 // const list = new LinkedList();
 // const node1 = new Node();
---
+++ LinkedList/practice.js
@@ -0,0 +1,5 @@
+let objeto1 = { a: 4 };
+let objeto2 = objeto1;
+objeto2.a = 10;
+console.log(objeto1);
+console.log(objeto2);
